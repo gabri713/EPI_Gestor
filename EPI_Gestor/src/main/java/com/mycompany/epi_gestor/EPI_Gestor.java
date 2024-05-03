939c7721e4da1d3d100a1800ddb5a74de1b7f4d1
gabri713 <gabrielr713@hotmail.com>
2024-05-03 20:55:02 -0300

    Criação repositorio

---
+++ EPI_Gestor/src/main/java/com/mycompany/epi_gestor/EPI_Gestor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.epi_gestor;
+
+/**
+ *
+ * @author g.pereira
+ */
+public class EPI_Gestor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
